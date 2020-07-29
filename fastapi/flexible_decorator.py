356a0b15bb67deaeb7f0350a1f48af27d7108cb5
Ben Falk <bfalk@stsci.edu>
2020-07-29 11:33:38 -0400

    flexible decorator working on fastapi methods

--- fastapi/flexible_decorator.py
+++ fastapi/flexible_decorator.py
@@ -30,7 +30,34 @@ def duration(func):
     return wrapper
 
 
-@duration
+def duration2(func):
+    """
+    decorator that can take either coroutine or normal function 
+    works on FastAPI methods as well
+    """
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start_ts = time.time()
+        result = func(*args, **kwargs)
+        dur = time.time() - start_ts
+        print('{} took {:.2} seconds'.format(func.__name__, dur))
+        return result
+
+    @functools.wraps(func)
+    async def async_wrapper(*args, **kwargs):
+        start_ts = time.time()
+        result = await func(*args, **kwargs)
+        dur = time.time() - start_ts
+        print('{} took {:.2} seconds'.format(func.__name__, dur))
+        return result
+
+    if asyncio.iscoroutinefunction(func):
+        return async_wrapper
+    else:
+        return wrapper
+
+
+@duration2
 def main(sleep_time=.5):
     print("normal function sleeps for:", sleep_time)
     time.sleep(sleep_time)
@@ -38,7 +65,7 @@ def main(sleep_time=.5):
     return
 
 
-@duration
+@duration2
 async def main_async(sleep_time=.75):
     print("coroutine sleeps for:", sleep_time)
     await asyncio.sleep(sleep_time)
--- fastapi/flexible_fastapi_decorator.py
+++ fastapi/flexible_fastapi_decorator.py
@@ -1,37 +1,41 @@
 """ flexible decorator around fastapi routes
-call with: `uvicorn flexible_fastapi_decorator:app --reload` or `python flexible_fastapi_decorator.py` """
+call with: `uvicorn flexible_fastapi_decorator:app --reload` or `python flexible_fastapi_decorator.py`
+stackoverflow: https://stackoverflow.com/q/44169998/532963
+"""
 
-import uvicorn
-from fastapi import FastAPI
 import asyncio
 import functools
 import time
-from contextlib import contextmanager
 
-"""
-from stackoverflow: https://stackoverflow.com/q/44169998/532963
-"""
+import aiofiles
+import uvicorn
+
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 
 
 def duration(func):
-    """ decorator that can take either coroutine or normal function """
-    @contextmanager
-    def wrapping_logic():
+    """adapted from github: 
+    https://gist.github.com/Integralist/77d73b2380e4645b564c28c53fae71fb/c2c757ff701aaf1f368a4aae232900cd846b425e#file-python-asyncio-timing-decorator-py
+    """
+
+    async def helper(func, *args, **kwargs):
+        if asyncio.iscoroutinefunction(func):
+            print(f"this function is a coroutine: {func.__name__}")
+            return await func(*args, **kwargs)
+        else:
+            print(f"not a coroutine: {func.__name__}")
+            return func(*args, **kwargs)
+
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
         start_ts = time.time()
-        yield
+        result = await helper(func, *args, **kwargs)
         dur = time.time() - start_ts
         print('{} took {:.2} seconds'.format(func.__name__, dur))
 
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        if not asyncio.iscoroutinefunction(func):
-            with wrapping_logic():
-                return func(*args, **kwargs)
-        else:
-            async def tmp():
-                with wrapping_logic():
-                    return (await func(*args, **kwargs))
-            return tmp()
+        return result
+
     return wrapper
 
 
@@ -40,10 +44,10 @@ app = FastAPI()
 
 @app.get("/hello")
 @duration
-def slow_hello(sleep_time=.5):
-    """ route that uses a flexible decorator """
+def slow_hello(sleep_time: float = .5):
+    """ normal function using a flexible decorator """
 
-    print("normal function sleeps for:", sleep_time)
+    print("normal function sleeping for:", sleep_time)
     time.sleep(sleep_time)
     print("normal waited")
     return {"message": "slow hello"}
@@ -51,11 +55,73 @@ def slow_hello(sleep_time=.5):
 
 @app.get("/async_hello")
 @duration
-async def slow_async_hello(sleep_time=.75):
-    print("coroutine sleeps for:", sleep_time)
+async def slow_async_hello(sleep_time: float = .75):
+    """ coroutine function using a flexible decorator """
+
+    print("coroutine sleeping for:", sleep_time)
     await asyncio.sleep(sleep_time)
     print('coroutine waited')
     return {"message": "slow async hello"}
+
+
+@app.get("/exception_async")
+@duration
+async def exception_async():
+    """ coroutine function try/catch example """
+
+    print("we're about to throw an exception")
+    try:
+        raise TypeError
+    except TypeError:
+        print("we raised an exception")
+        await asyncio.sleep(.25)
+    return {"message": "we got through the try/except"}
+
+
+@app.get("/exception")
+@duration
+def exception():
+    """ normal function try/catch example """
+
+    print("we're about to throw an exception from a normal method")
+    try:
+        raise ValueError
+    except ValueError:
+        print("we raised an exception")
+        time.sleep(.25)
+    return {"message": "we got through the try/except"}
+
+
+@app.get("/with_async")
+@duration
+async def with_method_async():
+    """ coroutine function using with block """
+
+    async def gen():
+        async with aiofiles.open(f"{__file__}") as f:
+            print("we are inside a with block")
+            async for line in f:
+                yield line
+
+    return StreamingResponse(gen())
+
+
+@app.get("/with")
+@duration
+def with_method():
+    """ normal function using with block """
+
+    print("normal function with block")
+
+    def gen():
+        with open(f"{__file__}") as f:
+            # this doesn't get printed to console until the end
+            print("we are inside a with block")
+            for line in f:
+                yield line
+
+    return StreamingResponse(gen())
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
