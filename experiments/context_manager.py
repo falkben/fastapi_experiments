ad59cb06da19c556d1775956caa62cbdcd8ee6ea
Ben Falk <bfalk@stsci.edu>
2020-08-20 14:36:11 -0400

    renamed the path operators for clarity
    added a few comments

--- experiments/context_manager.py
+++ experiments/context_manager.py
@@ -1,47 +1,59 @@
-from contextlib import contextmanager
 import time
-
-from fastapi.params import Depends
-from starlette.responses import StreamingResponse
+from contextlib import contextmanager
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.params import Depends
+from fastapi.responses import StreamingResponse
 
 app = FastAPI()
 
 
 @contextmanager
-def hello_context_manager():
+def simple_context_manager():
+    """ this could be a wrapper around a database """
+
     def _gen():
-        for i in range(1000):
+        iterations = 1000
+        for i in range(iterations):
             yield str(i)
             time.sleep(0.001)
+            if i == iterations - 1:
+                print("reached the end")
 
     try:
         yield _gen
     finally:
+        # we could be closing the database conn here
         print("we are cleaning up")
 
 
-def db_query():
+def mock_db_query():
     """ Dependency wrapping a context manager """
-    with hello_context_manager() as db:
+    with simple_context_manager() as db:
         yield db
 
 
-@app.get("/hello")
-async def hello():
-    with hello_context_manager() as gen:
-        # ! DO NOT USE THIS, USE DEPENDENCY INSTEAD
-        # ! this causes the finally block to occur immediately
+@app.get("/stream_context_mngr")
+async def stream_context_mngr():
+    with simple_context_manager() as gen:
+        # ! this causes the finally block in the context manager to occur immediately *before* streaming the response
         # returning StreamingResponse causes the context manager to close for some reason
         return StreamingResponse(gen())
 
 
-@app.get("/hello_dep")
-async def hello_dep(db=Depends(db_query)):
-    """ could be sync or async path operator here """
+@app.get("/stream_dep")
+async def stream_dep(db=Depends(mock_db_query)):
+    """ could be sync or async path operator here 
+    This works as expected.  Finally block in the "wrapped" context manager occurs after the response is finished """
     return StreamingResponse(db())
+
+
+@app.get("/direct_context_mngr")
+async def direct_context_mngr():
+    with simple_context_manager() as gen:
+        # ! This also seems to result in the finally block executing before the response is returned or the generator is consumed
+        return gen()
 
 
 if __name__ == "__main__":
