fb64ecf55466578684140e4cb48beaf3100cce14
Ben Falk <bfalk@stsci.edu>
2020-09-03 11:33:09 -0400

    "+" prefix to float q_param example

--- experiments/params.py
+++ experiments/params.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import uvicorn
 from fastapi import FastAPI, Query, Request
 
@@ -9,6 +11,12 @@ def hello(request: Request, sleep_time: int = Query(None)):
     """ Can get query args not defined as params """
     query_params = request.query_params.items()
     return query_params
+
+
+@app.get("/float")
+def float_func(float_p: Optional[float] = Query(None)):
+    """ Can get query args not defined as params """
+    return float_p
 
 
 if __name__ == "__main__":
--- experiments/test_params.py
+++ experiments/test_params.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi.testclient import TestClient
 
 from experiments.params import app
@@ -9,3 +10,10 @@ def test_hello():
     resp = client.get("/hello?sleep_time=45")
     assert resp.status_code == 200
     assert resp.json() == {"sleep_time": "45"}
+
+
+@pytest.mark.parametrize("param", ["+1.2", 1.2])
+def test_float(param):
+    resp = client.get(f"/float?float_p={param}")
+    assert resp.status_code == 200
+    assert resp.json() == float(param)
