085b5008ce7b957f6adc6c72925f493d6aa38154
Ben Falk <bfalk@stsci.edu>
2020-12-02 23:16:22 -0500

    json -> redirect to form using exception_handler

--- experiments/form_json_body.py
+++ experiments/form_json_body.py
@@ -1,5 +1,7 @@
 import uvicorn
-from fastapi import FastAPI, Form, Depends, Header, Request, Response
+from fastapi import Depends, FastAPI, Form, Header, Query, Request, Response, params
+from fastapi.exception_handlers import request_validation_exception_handler
+from fastapi.exceptions import RequestValidationError
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 
@@ -17,6 +19,17 @@ def form_body(cls):
     return cls
 
 
+def query_param(cls):
+    # from https://github.com/tiangolo/fastapi/issues/1989#issuecomment-684799715
+    cls.__signature__ = cls.__signature__.replace(
+        parameters=[
+            arg.replace(default=Query(...))
+            for arg in cls.__signature__.parameters.values()
+        ]
+    )
+    return cls
+
+
 class UserData(BaseModel):
     name: str
     greeting: str
@@ -27,30 +40,77 @@ class UserFormData(UserData):
     ...
 
 
+@query_param
+class UserQueryData(UserData):
+    ...
+
+
 async def redirect_to_json(content_type: str = Header(None)):
     if content_type == "application/json":
-        raise RedirectJSONException
+        raise RedirectException("/json")
+
+
+async def redirect_to_form(content_type: str = Header(None)):
+    if content_type != "application/json":
+        raise RedirectException("/form")
 
 
 @app.post("/form", dependencies=[Depends(redirect_to_json)])
 def form_endpoint(
-    user: UserFormData = Depends(UserFormData),
+    user: UserData = Depends(UserFormData),
 ):
     return user
 
 
-@app.post("/json")
+@app.post("/json", dependencies=[Depends(redirect_to_form)])
 def json_endpoint(user: UserData):
     return user
 
 
-class RedirectJSONException(Exception):
-    pass
+@app.get("/query")
+def query_endpoint(user: UserData = Depends(UserQueryData)):
+    return user
 
 
-@app.exception_handler(RedirectJSONException)
-async def exception_handler(request: Request, exc: RedirectJSONException) -> Response:
-    return RedirectResponse(url="/json")
+class RedirectException(Exception):
+    def __init__(self, url: str) -> None:
+        self.url = url
+
+
+@app.exception_handler(RedirectException)
+async def exception_handler(request: Request, exc: RedirectException) -> Response:
+    return RedirectResponse(url=exc.url)
+
+
+@app.exception_handler(RequestValidationError)
+async def json_request_validation_exception_handler(
+    request: Request, exc: RequestValidationError
+) -> Response:
+
+    # get the current route of the request
+    # then check the body_field of the route (as in fastapi.routing):
+    # `body_field and isinstance(body_field.field_info, params.Form)`
+    routes = [r.path for r in request.app.routes]
+    body_field = None
+    if request.url.path in routes:
+        route_index = routes.index(request.url.path)
+        # body_field is None for get requests
+        body_field = request.app.routes[route_index].body_field
+
+    if (
+        body_field
+        and not isinstance(body_field.field_info, params.Form)
+        and request.headers.get("content-type") != "application/json"
+        # we could check for a specific endpoint:
+        # request.url.path == "/json"
+        # could also check for the type of error:
+        # exc.errors()[0]["type"] == "value_error.jsondecode"
+    ):
+        # we should redirect to the form endpoint
+        return RedirectResponse(url="/form")
+    else:
+        # otherwise we just have a bad request and we do the normal thing
+        return await request_validation_exception_handler(request, exc)
 
 
 if __name__ == "__main__":
--- experiments/test_form_json_body.py
+++ experiments/test_form_json_body.py
@@ -20,8 +20,44 @@ def test_form():
     assert resp.status_code == 200
     assert resp.json() == data
 
+    # test a basic validation error
+    stripped_data = data.copy()
+    stripped_data.pop("name")  # name is a required param
+    resp = client.post("/form", data=stripped_data)
+    assert resp.status_code == 422
+
 
 def test_json():
+    # test send json data to json endpoint (normal)
     resp = client.post("/json", json=data)
     assert resp.status_code == 200
     assert resp.json() == data
+
+    # test a basic data validation error
+    stripped_data = data.copy()
+    stripped_data.pop("name")  # name is a required param
+    resp = client.post("/json", json=stripped_data)
+    assert resp.status_code == 422
+
+    # test sending form data to json endpoint, check for redirect
+    resp = client.post("/json", data=data)
+    assert resp.status_code == 307
+    assert resp.headers.get("location") == "/form"
+
+    # test sending form data to json endpoint, allowing redirect
+    resp = client.post("/json", data=data, allow_redirects=True)
+    assert resp.status_code == 200
+    assert resp.json() == data
+
+
+def test_query():
+    # test sending form data to json endpoint, allowing redirect
+    query_args = "&".join([f"{k}={v}" for k, v in data.items()])
+    resp = client.get(f"/query?{query_args}")
+    assert resp.status_code == 200
+    assert resp.json() == data
+
+    # validation error here
+    query_args = "&".join([[f"{k}={v}" for k, v in data.items()][0]])
+    resp = client.get(f"/query?{query_args}")
+    assert resp.status_code == 422
