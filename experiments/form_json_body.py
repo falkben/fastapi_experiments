fd92ec7480e35959442aae2f24eebbcfcd7db451
Ben Falk <bfalk@stsci.edu>
2020-12-07 15:02:22 -0500

    added the custom route for form -> body

--- experiments/form_json_body.py
+++ experiments/form_json_body.py
@@ -1,33 +1,126 @@
+import json
+from typing import Callable
+
+import fastapi.params
 import uvicorn
-from fastapi import Depends, FastAPI, Form, Header, Query, Request, Response, params
+from fastapi import (
+    APIRouter,
+    Depends,
+    FastAPI,
+    Form,
+    Header,
+    Query,
+    Request,
+    Response,
+    params,
+)
 from fastapi.exception_handlers import request_validation_exception_handler
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import RedirectResponse
+from fastapi.routing import APIRoute
 from pydantic import BaseModel
+from starlette.routing import Match
+
+
+class FormToJSONRequest(Request):
+    """Custom request which converts form input to JSON for JSON post endpoints
+    It reads content-type header of request to see if it's form data
+    If it's a form request it checks the matched route and converts the form data
+    to JSON"""
+
+    async def body(self) -> bytes:
+        """ Overwrite body method """
+        if not hasattr(self, "_body"):
+
+            # only if request content-type is form
+            if self.headers.get("content-type", None) in [
+                "multipart/form-data",
+                "application/x-www-form-urlencoded",
+            ]:
+
+                # find matching route and body_field
+                body_field = None
+                for route in self.app.routes:
+                    match, _ = route.matches(self.scope)
+                    if match == Match.FULL:
+                        body_field = route.body_field
+                        break
+
+                # if matching route has body field & is not form
+                if body_field and not isinstance(
+                    body_field.field_info, fastapi.params.Form
+                ):
+                    form = await self.form()
+                    del self._form
+                    form_dict = dict(form)
+                    body = json.dumps(form_dict).encode("utf-8")
+                    self._body = body
+                    # we might as well store it now while it's a dict
+                    self._json = form_dict
+                    return self._body
+
+            body = await super().body()
+            self._body = body
+
+        return self._body
+
+
+class FormToJSONRoute(APIRoute):
+    """Custom Route Class for converting Form data to JSON
+
+    Example:
+
+    ```python
+    router = APIRouter()
+    router.route_class = util.FormToJSONRoute
+    ```
+
+    Any @router.post path operators that take JSON data will have form input
+    automatically converted to JSON
+
+    See
+    https://fastapi.tiangolo.com/advanced/custom-request-and-route/#handling-custom-request-body-encodings
+    """
+
+    def get_route_handler(self) -> Callable:
+        """ overwrite get_route_handler with FormToJSONRequest """
+        original_route_handler = super().get_route_handler()
+
+        async def custom_route_handler(request: Request) -> Response:
+            request = FormToJSONRequest(request.scope, request.receive)
+            return await original_route_handler(request)
+
+        return custom_route_handler
+
 
 app = FastAPI()
+
+# isolating the redirect exception handling from form parsing
+router = APIRouter()
+
+
+def modify_field_signature(cls, field_type):
+    cls.__signature__ = cls.__signature__.replace(
+        parameters=[
+            arg.replace(
+                default=field_type(...)
+                if arg.default is arg.empty
+                else field_type(arg.default)
+            )
+            for arg in cls.__signature__.parameters.values()
+        ]
+    )
+    return cls
 
 
 def form_body(cls):
     # from https://github.com/tiangolo/fastapi/issues/1989#issuecomment-684799715
-    cls.__signature__ = cls.__signature__.replace(
-        parameters=[
-            arg.replace(default=Form(...))
-            for arg in cls.__signature__.parameters.values()
-        ]
-    )
-    return cls
+    return modify_field_signature(cls, Form)
 
 
 def query_param(cls):
     # from https://github.com/tiangolo/fastapi/issues/1989#issuecomment-684799715
-    cls.__signature__ = cls.__signature__.replace(
-        parameters=[
-            arg.replace(default=Query(...))
-            for arg in cls.__signature__.parameters.values()
-        ]
-    )
-    return cls
+    return modify_field_signature(cls, Query)
 
 
 class UserData(BaseModel):
@@ -50,19 +143,19 @@ async def redirect_to_json(content_type: str = Header(None)):
         raise RedirectException("/json")
 
 
-@app.post("/form", dependencies=[Depends(redirect_to_json)])
+@router.post("/form", dependencies=[Depends(redirect_to_json)])
 def form_endpoint(
     user: UserData = Depends(UserFormData),
 ):
     return user
 
 
-@app.post("/json")
+@router.post("/json")
 def json_endpoint(user: UserData):
     return user
 
 
-@app.get("/query")
+@router.get("/query")
 def query_endpoint(user: UserData = Depends(UserQueryData)):
     return user
 
@@ -107,6 +200,29 @@ async def json_request_validation_exception_handler(
         # otherwise we just have a bad request and we do the normal thing
         return await request_validation_exception_handler(request, exc)
 
+
+# isolating on a separater router to make testing a little more obvious
+form_to_json_router = APIRouter(prefix="/form_to_json")
+form_to_json_router.route_class = FormToJSONRoute
+
+
+@form_to_json_router.post("/form")
+def form_to_json_form_endpoint(user: UserData = Depends(UserFormData)):
+    return user
+
+
+@form_to_json_router.post("/json")
+def form_to_json_json_endpoint(user: UserData):
+    return user
+
+
+@form_to_json_router.get("/query")
+def form_to_json_query_endpoint(user: UserData = Depends(UserQueryData)):
+    return user
+
+
+app.include_router(router)
+app.include_router(form_to_json_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- experiments/test_form_json_body.py
+++ experiments/test_form_json_body.py
@@ -7,18 +7,21 @@ client = TestClient(app)
 data = {"name": "bob", "greeting": "hi"}
 
 
+def assert_good_response(resp):
+    assert resp.status_code == 200
+    assert resp.json() == data
+
+
 def test_form():
     resp = client.post("/form", json=data)
     assert resp.status_code == 307
     assert resp.headers.get("location") == "/json"
 
     resp = client.post("/form", json=data, allow_redirects=True)
-    assert resp.status_code == 200
-    assert resp.json() == data
+    assert_good_response(resp)
 
     resp = client.post("/form", data=data)
-    assert resp.status_code == 200
-    assert resp.json() == data
+    assert_good_response(resp)
 
     # test a basic validation error
     stripped_data = data.copy()
@@ -30,8 +33,7 @@ def test_form():
 def test_json():
     # test send json data to json endpoint (normal)
     resp = client.post("/json", json=data)
-    assert resp.status_code == 200
-    assert resp.json() == data
+    assert_good_response(resp)
 
     # test a basic data validation error
     stripped_data = data.copy()
@@ -46,18 +48,61 @@ def test_json():
 
     # test sending form data to json endpoint, allowing redirect
     resp = client.post("/json", data=data, allow_redirects=True)
-    assert resp.status_code == 200
-    assert resp.json() == data
+    assert_good_response(resp)
 
 
 def test_query():
     # test sending form data to json endpoint, allowing redirect
     query_args = "&".join([f"{k}={v}" for k, v in data.items()])
     resp = client.get(f"/query?{query_args}")
-    assert resp.status_code == 200
-    assert resp.json() == data
+    assert_good_response(resp)
 
     # validation error here
     query_args = "&".join([[f"{k}={v}" for k, v in data.items()][0]])
     resp = client.get(f"/query?{query_args}")
     assert resp.status_code == 422
+
+
+# test form_to_json router
+prefix = "/form_to_json"
+
+
+def test_form_to_json_json():
+    # test sending form data to json endpoint, check for redirect
+    resp = client.post(prefix + "/json", data=data)
+    assert_good_response(resp)
+
+    # test send json data to json endpoint (normal)
+    resp = client.post(prefix + "/json", json=data)
+    assert_good_response(resp)
+
+    # test a basic data validation error
+    # name is a required param
+    stripped_data = {k: v for k, v in data.items() if k != "name"}
+    resp = client.post(prefix + "/json", json=stripped_data)
+    assert resp.status_code == 422
+
+    # test sending form data to json endpoint, allowing redirect
+    resp = client.post(prefix + "/json", data=data, allow_redirects=True)
+    assert_good_response(resp)
+
+
+def test_form_to_json_form():
+
+    resp = client.post("/form", data=data)
+    assert_good_response(resp)
+
+    # test a basic validation error
+    # name is a required param
+    stripped_data = {k: v for k, v in data.items() if k != "name"}
+    resp = client.post("/form", data=stripped_data)
+    assert resp.status_code == 422
+
+    # since the exception handler is on the app this still redirects
+    resp = client.post("/form", json=data)
+    assert resp.status_code == 307
+    assert resp.headers.get("location") == "/json"
+
+    # since the exception handler is on the app this redirects and works
+    resp = client.post("/form", json=data, allow_redirects=True)
+    assert_good_response(resp)
