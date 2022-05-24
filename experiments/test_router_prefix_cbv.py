6c352f0ef56a00f2a50dc416700b2ac888fcad4a
Ben Falk <bfalk@stsci.edu>
2022-05-24 17:04:52 -0400

    update test to demonstrate that fastapi_utils double adds prefixes

--- experiments/test_router_prefix_cbv.py
+++ experiments/test_router_prefix_cbv.py
@@ -1,19 +1,20 @@
-""" Both fastapi_utils and fastapi_restful double add prefixes """
+""" fastapi_utils double adds prefixes """
 
 from fastapi import APIRouter
+from fastapi_utils.cbv import cbv
 
-# from fastapi_utils.cbv import cbv
-from fastapi_restful.cbv import cbv
+# fastapi_restful does not double add prefixes
+# from fastapi_restful.cbv import cbv
 
 router = APIRouter(prefix="/api/v1")
 
 
 @cbv(router)
 class C:
-    @router.get("")
+    @router.get("/")
     def f(self):
         ...
 
 
 def test_router_prefix():
-    assert router.routes[-1].path == "/api/v1/api/v1"
+    assert router.routes[-1].path == "/api/v1/api/v1/"
